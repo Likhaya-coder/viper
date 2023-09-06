e7857b53761c700953094152cfdc57568aaaf4e5
Likhaya-coder <llikhaya.kulati@gmail.com>
2023-09-06 17:25:56 +0200

    added login and register screens

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vapor/constants.dart';
+import 'package:vapor/pages/register.dart';
 import 'package:vapor/utilities/glassmorphism.dart';
 
 class Login extends StatefulWidget {
@@ -15,29 +16,18 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      body: SizedBox(
         width: double.infinity,
         height: double.infinity,
         child: Glassmorphism(
             child: SingleChildScrollView(
           child: Column(
             children: [
-              Icon(
+              const Icon(
                 Icons.car_crash,
                 size: 160.0,
                 color: Colors.redAccent,
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: TextField(
-                  cursorColor: Colors.black,
-                  style: const TextStyle(color: Colors.black54),
-                  decoration: inputDecoration.copyWith(
-                    hintText: 'Enter your username',
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10.0),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 8.0),
                 child: TextField(
@@ -59,28 +49,44 @@ class _LoginState extends State<Login> {
                   ),
                 ),
               ),
-              const SizedBox(height: 10.0),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: TextField(
-                  cursorColor: Colors.black,
-                  style: const TextStyle(color: Colors.black54),
-                  decoration: inputDecoration.copyWith(
-                    hintText: 'Repeat your password',
-                  ),
+                padding: const EdgeInsets.symmetric(
+                  vertical: 20.0,
+                  horizontal: 8.0,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    const Text(
+                      'Don\'t have an account? ',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, Register.id);
+                      },
+                      child: const Text(
+                        ' Register now',
+                        style: TextStyle(
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              const SizedBox(height: 10.0),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                  minimumSize: Size(
+                  minimumSize: const Size(
                     double.infinity,
                     70.0,
                   ),
                   backgroundColor: Colors.redAccent,
                 ),
                 onPressed: () {},
-                child: Text('Register'),
+                child: const Text('Login'),
               ),
             ],
           ),
--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:vapor/constants.dart';
+import 'package:vapor/pages/login.dart';
+
+import '../utilities/glassmorphism.dart';
 
 class Register extends StatefulWidget {
   const Register({super.key});
@@ -12,6 +16,106 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Glassmorphism(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const Icon(
+                  Icons.car_crash,
+                  size: 160.0,
+                  color: Colors.redAccent,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: TextField(
+                    cursorColor: Colors.black,
+                    style: const TextStyle(color: Colors.black54),
+                    decoration: inputDecoration.copyWith(
+                      hintText: 'Enter your username',
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10.0),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: TextField(
+                    cursorColor: Colors.black,
+                    style: const TextStyle(color: Colors.black54),
+                    decoration: inputDecoration.copyWith(
+                      hintText: 'Enter your email',
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10.0),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: TextField(
+                    cursorColor: Colors.black,
+                    style: const TextStyle(color: Colors.black54),
+                    decoration: inputDecoration.copyWith(
+                      hintText: 'Enter your password',
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10.0),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: TextField(
+                    cursorColor: Colors.black,
+                    style: const TextStyle(color: Colors.black54),
+                    decoration: inputDecoration.copyWith(
+                      hintText: 'Repeat your password',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 20.0,
+                    horizontal: 8.0,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const Text(
+                        'Already a member? ',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, Login.id);
+                        },
+                        child: const Text(
+                          ' Sign in here',
+                          style: TextStyle(
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(
+                      double.infinity,
+                      70.0,
+                    ),
+                    backgroundColor: Colors.redAccent,
+                  ),
+                  onPressed: () {},
+                  child: const Text('Register'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
